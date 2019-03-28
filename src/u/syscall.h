04e62ecc60ca5c8477b2e5dc150e7d887f016658
otamajakusi <hiroyuki.obinata@gmail.com>
2019-03-28 22:23:34 +0900

    fix syscall

--- src/u/syscall.h
+++ src/u/syscall.h
@@ -11,6 +11,22 @@
 #define SYSCALL_CLOSE   3
 
 #if !defined(__ASSEMBLER__)
-int __read(int fd, void *buf, size_t count);
-int __write(int fd, const void *buf, size_t count);
+#define __SYSCALL0(number)                  __syscall0((number))
+#define __SYSCALL1(number, a0)              __syscall1((number), (int)(a0))
+#define __SYSCALL2(number, a0, a1)          __syscall2((number), (int)(a0), (int)(a1))
+#define __SYSCALL3(number, a0, a1, a2)      __syscall3((number), (int)(a0), (int)(a1), (int)(a2))
+
+int __syscall0(int number);
+int __syscall1(int number, int a0);
+int __syscall2(int number, int a0, int a1);
+int __syscall3(int number, int a0, int a1, int a2);
+
+static inline int __read(int fd, void *buf, size_t count) {
+    return __SYSCALL3(SYSCALL_READ, fd, buf, count);
+}
+
+static inline int __write(int fd, const void *buf, size_t count) {
+    return __SYSCALL3(SYSCALL_WRITE, fd, buf, count);
+}
+
 #endif
