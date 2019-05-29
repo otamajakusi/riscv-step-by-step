482e8b049a0b81beb7efd2eba6e66642c006b6e3
otamajakusi <hiroyuki.obinata@gmail.com>
2019-05-29 09:12:10 +0900

    add run-gdb target

--- mk/m.mk
+++ mk/m.mk
@@ -4,6 +4,11 @@ LDFLAGS += -L$(MAKEFILE_DIR)../riscv-probe/env/qemu-sifive_u -Tdefault.lds \
 run:
 	qemu-system-riscv32 -nographic -machine sifive_u -kernel $(target)
 
+# `run-gdb` and
+# `riscv32-unknown-elf-gdb -ex "target remote localhost:1234"` from another terminal.
+run-gdb:
+	qemu-system-riscv32 -nographic -machine sifive_u -kernel $(target) -s -S
+
 .PHONY: stop
 stop:
 	killall qemu-system-riscv32
