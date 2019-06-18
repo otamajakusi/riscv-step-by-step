cac84b5c22f0fa5d78264c565f37712d7b346931
otamajakusi <hiroyuki.obinata@gmail.com>
2019-06-18 22:17:09 +0900

    fix read system call. step11 impl is done.

--- steps/11/m/read.c
+++ steps/11/m/read.c
@@ -1,24 +1,13 @@
+/*
+ * 1. system call (handle_read)
+ * 2. receive data (receive_read_data)
+ * 3. retrieve data (retrieve_read_data)
+ */
 #include <stdio.h>
 #include "read.h"
 #include "sched.h"
 
-#define BUF_SIZE    (32)
-
-static char buf[BUF_SIZE];
-static int wp = 0;
-static int rp = 0;
-
 static struct task_t* queue = NULL;
-
-static int retrieve_read_data()
-{
-    if (rp == wp) {
-        return -1;
-    }
-    int c = buf[rp];
-    rp = (rp + 1) % BUF_SIZE;
-    return c;
-}
 
 static void enqueue(task_t *curr)
 {
@@ -44,23 +33,19 @@ static task_t* dequeue()
     return p;
 }
 
-int is_read_queue_empty()
+static int retrieve_read_data(uintptr_t* regs, task_t* p, int c)
 {
-    return queue == NULL;
+    uintptr_t va = regs[REG_CTX_A2];
+    uintptr_t pa = va_to_pa(p->pte, va, 0);
+    *(char*)(PAGE_OFFSET(va) + pa) = c;
+    regs[REG_CTX_A0] = 1; // return size
+    return 0;
 }
 
 void handle_read(uintptr_t* regs, task_t* curr)
 {
-    uintptr_t va = regs[REG_CTX_A2];
-    uintptr_t pa = va_to_pa(curr->pte, va, 0);
-    char *d = (char*)(PAGE_OFFSET(va) + pa);
-    int c = retrieve_read_data();
-    if (c == -1) {
-        enqueue(curr);
-        return;
-    }
-    *d = c;
-    return;
+    (void)regs;
+    enqueue(curr);
 }
 
 int receive_read_data(char c)
@@ -68,16 +53,9 @@ int receive_read_data(char c)
     if (queue == NULL) {
         return -1;
     }
-    dequeue();
 
-    int wp_next = (wp + 1) % BUF_SIZE;
-    if (wp_next == rp) {
-        printf("warning: buf is full\n");
-        return 0;
-    }
-
-    buf[wp] = c;
-    wp = wp_next;
+    task_t* p = dequeue();
+    retrieve_read_data(p->regs, p, c);
     return 0;
 }
 
