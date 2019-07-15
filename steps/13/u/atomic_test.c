b56465e48e17c59ffe7ab300c9f567b6d3ee22ae
otamajakusi <hiroyuki.obinata@gmail.com>
2019-07-15 16:15:42 +0900

    update atomic_test

--- steps/13/u/atomic_test.c
+++ steps/13/u/atomic_test.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "atomic.h"
 
-static int __attribute__ ((noinline))
+int __attribute__ ((noinline))
     _lr_sc_test_1(uint32_t *addr, uint32_t expected, uint32_t desired)
 {
-    (void)addr;
     (void)expected;
     (void)desired;
     asm volatile (
@@ -17,10 +17,23 @@ static int __attribute__ ((noinline))
 
 }
 
-static int __attribute__ ((noinline))
+int __attribute__ ((noinline))
     _lr_sc_test_2(uint32_t *addr, uint32_t expected, uint32_t desired)
 {
-    (void)addr;
+    (void)expected;
+    (void)desired;
+    asm volatile (
+            "lr.w    a3, (a0);"
+            "sw      a2, 0(a0);"
+            "sc.w    a0, a2, (a0);"
+            "seqz    a0, a0;");
+    return (int)addr;
+
+}
+
+int __attribute__ ((noinline))
+    _lr_sc_test_3(uint32_t *addr, uint32_t expected, uint32_t desired)
+{
     (void)expected;
     (void)desired;
     asm volatile (
@@ -30,26 +43,54 @@ static int __attribute__ ((noinline))
     return (int)addr;
 }
 
+#define EXPECT_EQ(a, b) { \
+    if ((int)(a) != (int)(b)) { \
+        printf("%d: \""#a"\" != \""#b"\"\n", __LINE__); \
+        exit(1); \
+    } else { \
+        putchar('+'); \
+    } \
+}
+
 void atomic_test()
 {
     int ret;
     uint32_t val;
+
     val = 0;
     ret = _lr_sc_test_1(&val, 0, 1);
-    printf("ret %d, expect 0, val %lx, expect %p\n", ret, val, &val);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(val, &val);
+
     val = 0;
-    ret = _lr_sc_test_2(&val, 0, 1);
-    printf("ret %d, expect 1, val %ld, expect 1\n", ret, val);
+    ret = _lr_sc_test_2(&val, 0, 0xdeadbeef);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(val, 0xdeadbeef);
+
+    val = 0;
+    ret = _lr_sc_test_3(&val, 0, 1);
+    EXPECT_EQ(ret, 1);
+    EXPECT_EQ(val, 1);
+
     val = 0;
     ret = _atomic_compare_exchange(&val, 100, 200);
-    printf("ret %d, expect 0, val %ld, expect 0\n", ret, val);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(val, 0);
+
     val = 100;
     ret = _atomic_compare_exchange(&val, 100, 200);
-    printf("ret %d, expect 1, val %ld, expect 200\n", ret, val);
+    EXPECT_EQ(ret, 1);
+    EXPECT_EQ(val, 200);
+
     val = 100;
     ret = _atomic_exchange(&val, 200);
-    printf("ret %d, expect 100, val %ld, expect 200\n", ret, val);
+    EXPECT_EQ(ret, 100);
+    EXPECT_EQ(val, 200);
+
     val = 300;
     ret = _atomic_exchange(&val, 400);
-    printf("ret %d, expect 300, val %ld, expect 400\n", ret, val);
+    EXPECT_EQ(ret, 300);
+    EXPECT_EQ(val, 400);
+
+    printf("%s: done\n", __func__);
 }
